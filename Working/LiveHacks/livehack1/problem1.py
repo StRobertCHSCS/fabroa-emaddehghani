bbb53e4aeabac02d7fa1c80162375a207280404c
emaddehghani <mohammademad.dehghani22@ycdsbk12.ca>
2019-10-02 09:10:07 -0400

    live hack 1

---
+++ Working/LiveHacks/livehack1/problem1.py
@@ -0,0 +1,14 @@
+-------------------------------------------------------------------------------
+Name:		practice1.py
+Purpose:	Compute the area and perimeter of a rectangle
+
+Author:		dehghani.E
+
+Created:	2/10/2019
+-------------------------------------------------------------------------------
+# get celcius from user
+celcius = float(input ("enter celcius: "))
+# cumpute fahrenhiet from celcius 
+fahrenhiet = 9/5 * (celcius + 32)
+# output fahrenhiet 
+print("the temputure in fahrenheit is " + str(fahrenheit))
