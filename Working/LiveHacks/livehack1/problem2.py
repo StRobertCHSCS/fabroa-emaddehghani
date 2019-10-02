d852da3e3e0d9f91941713f059661a8ffd3f2968
emaddehghani <mohammademad.dehghani22@ycdsbk12.ca>
2019-10-02 09:23:35 -0400

    live hack 2

---
+++ Working/LiveHacks/livehack1/problem2.py
@@ -0,0 +1,6 @@
+# get amount of chicken peice and students from user
+students = float(input ("enter students: "))
+# copute chicken peice per studnet
+chicken = (student + 1)
+# output how many chicken peices every studnt gets 
+print("the amount of chiken peices per student is " + str(peices))
