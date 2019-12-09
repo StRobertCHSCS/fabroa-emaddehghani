c2379e3197bf6f52e273b1264e8f9f939bf9dafa
emaddehghani <mohammademad.dehghani22@ycdsbk12.ca>
2019-12-09 09:41:26 -0500

    live hack 3

---
+++ Working/LiveHacks/live hack 3/problem2.py
@@ -0,0 +1,26 @@
+-------------------------------------------------------------------------------
+Name:		livehack3_pronlem2
+Purpose:	calculate distance
+
+Author:		dehghani.E
+
+Created:    9/12/2019
+-------------------------------------------------------------
+ ***** Welcome to the Distance Tracker ****** 
+#enter the distance traveled for the day
+distance today = int(input("Enter a number: "))
+
+#add the number to the sum of the distance traveled for the days before
+print("disnatance today + last days")
+#compare the nswer to 100 
+overall_distance = 100
+#if the number bigger than 100 say ....
+if overall_distance > 100
+if overall_distance > 100
+print("you have reached 100km")  
+#if not save the number to add to tommorrows distance
+elif overall_distance == 100
+print("you have reached 100km")  
+ else 
+ print("you have yet to reach 100km")
+
